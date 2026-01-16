85fd9abbbd1676595e343506230475e38f0f77fa
Jose Garcia <joseluisgraa@gmail.com>
2026-01-16 12:46:13 +0000

    Added two routes to force 500 error response and an error log

--- src/server/router.js
+++ src/server/router.js
@@ -4,6 +4,8 @@ import { home } from './home/index.js'
 import { about } from './about/index.js'
 import { health } from './health/index.js'
 import { serveStaticFiles } from './common/helpers/serve-static-files.js'
+import { forceHttpError } from './force-http-error/index.js'
+import { forceErrorLog } from './force-error-log/index.js'
 
 export const router = {
   plugin: {
@@ -15,7 +17,7 @@ export const router = {
       await server.register([health])
 
       // Application specific routes, add your own routes here
-      await server.register([home, about])
+      await server.register([home, about, forceHttpError, forceErrorLog])
 
       // Static assets
       await server.register([serveStaticFiles])
